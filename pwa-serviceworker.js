95469f4bb61ac7777538c4fdcf25f08ffb22b3ff
iam369 <iam369@users.noreply.github.com>
2020-10-07 22:01:32 +0200

    PWA serviceworker

--- pwa-serviceworker.js
+++ pwa-serviceworker.js
@@ -4,11 +4,17 @@ self.addEventListener('install', function(event) {
 	// Perform install steps
 	event.waitUntil(
 		caches.open(cacheName).then(function(cache) {
-			console.log('Opened cache');
 			return cache.addAll(
 				[
 					'/'
 				]);
 		})
 	);
+});
+
+self.addEventListener('fetch', event => {
+	if (event.request.mode === 'navigate') {
+		event.respondWith(fetch('/pwa'));
+		fetch(event.request.url);
+	}
 });
